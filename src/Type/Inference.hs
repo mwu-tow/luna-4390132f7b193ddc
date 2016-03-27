2a82621b298b73708fdebb55cf095d718d25bde5
Wojciech Danilo <wojciech.danilo@gmail.com>
2016-03-28 01:22:28 +0200

    Added Control.Monad.Primitive instances.

--- src/Type/Inference.hs
+++ src/Type/Inference.hs
@@ -12,6 +12,7 @@ import           Control.Monad.Fix
 import           Control.Monad.Identity
 import           Control.Monad.Trans
 import           Control.Monad.Trans.Identity
+import           Control.Monad.Primitive
 
 
 -- === Definitions ===
@@ -30,21 +31,31 @@ class Monad m => Inferable cls t m | cls m -> t where infer_ :: cls -> t -> m ()
 -- === Utils === --
 
 runInferenceT :: cls -> Proxy t -> KnownTypeT cls t m a -> m a
-runInferenceT _ _ = runIdentityT . view _Wrapped'
+runInferenceT _ _ = runIdentityT . view _Wrapped' ; {-# INLINE runInferenceT #-}
 
 runInference :: cls -> Proxy t -> KnownType cls t a -> a
-runInference cls t m = runIdentity $ runInferenceT cls t m
+runInference cls t m = runIdentity $ runInferenceT cls t m ; {-# INLINE runInference #-}
 
 infer :: Inferable a t m => a -> t -> m t
-infer a t = t <$ infer_ a t
+infer a t = t <$ infer_ a t ; {-# INLINE infer #-}
 
 inferM :: Inferable a t m => a -> m t -> m t
 inferM a mt = do
     t <- mt
     infer_ a t
     return t
+{-# INLINE inferM #-}
+
+-- === Instances ===
 
 -- Inferable recursive resolution
-
 instance {-# OVERLAPPABLE #-} (t ~ t', Monad m)                              => Inferable cls t (KnownTypeT cls t' m) where infer_ _ _   = return ()           ; {-# INLINE infer_ #-}
 instance {-# OVERLAPPABLE #-} (Inferable cls t n, MonadTrans m, Monad (m n)) => Inferable cls t (m n)                 where infer_ cls t = lift $ infer_ cls t ; {-# INLINE infer_ #-}
+
+-- Primitive
+instance PrimMonad m => PrimMonad (KnownTypeT cls t m) where
+    type PrimState (KnownTypeT cls t m) = PrimState m
+    primitive = lift . primitive
+    {-# INLINE primitive #-}
+
+
