c3f83b2cc4efdbe0bbd036acf3d06809e4e139f8
Wojciech Danilo <wojciech.danilo@gmail.com>
2016-03-30 08:38:19 +0200

    Some new type-level container instances

--- src/Type/List.hs
+++ src/Type/List.hs
@@ -27,7 +27,7 @@ type family FromLst a where FromLst (Lst l) = l
 
 type family Removed    (el :: e)    (cont :: c) :: l
 type family RemovedIdx (idx :: Nat) (cont :: c) :: l
-type family ElAt       (idx :: Nat) (cont :: c) :: l 
+type family ElAt       (idx :: Nat) (cont :: c) :: l
 
 type instance Removed (el :: e) ((l ': ls) :: [e]) = If (el == l) ls (l ': Removed el ls)
 
@@ -67,8 +67,8 @@ type instance Append a '[]       = '[a]
 type instance Append a (l ': ls) = l ': Append a ls
 
 
-type instance Concat (lst :: [k]) ('[]       :: [k]) = lst
-type instance Concat (lst :: [k]) ((l ': ls) :: [k]) = Concat (Append l lst) ls
+type instance Concat ('[] :: [k])        (lst :: [k]) = lst
+type instance Concat ( (l ': ls) :: [k]) (lst :: [k]) = l ': Concat ls lst
 
 
 type instance Remove a '[] = '[]
@@ -89,7 +89,7 @@ type family Head' lst where Head' (a ': as) = a
 
 --type instance Unique (lst :: [k]) = Set.ToList (Set.AsSet lst)
 
--- FIXME[WD]: The following is just an fix for using Unique. 
+-- FIXME[WD]: The following is just an fix for using Unique.
 --            Due to lack of time this ugly fix has no background - probably some PolyKinded problem.
 type family UniqueFix lst where UniqueFix '[] = '[]
                                 UniqueFix lst = Unique lst
@@ -98,13 +98,17 @@ type instance Unique (lst :: [k]) = Set.ToList (Set.AsSet lst)
 type instance Reverse (lst :: [k]) = Reverse' lst '[]
 
 type family Reverse' lst lst' where
-    Reverse' '[] lst = lst
+    Reverse' '[]       lst = lst
     Reverse' (l ': ls) lst = Reverse' ls (l ': lst)
 
 
 type family Take (n :: Nat) lst where
-	Take 0 lst       = '[]
-	Take n (l ': ls) = l ': Take (n - 1) ls
+    Take 0 lst       = '[]
+    Take n (l ': ls) = l ': Take (n - 1) ls
+
+type family Drop (n :: Nat) lst where
+    Drop 0 lst       = lst
+    Drop n (l ': ls) = Drop (n - 1) ls
 
 --type instance Index (a :: *) (Recursive ((l ': ls) ::  [*] )) = If (a == l) (Just 0) (SuccMaybe (Index a (Recursive ls)))
 --type instance Index (a :: *) (Recursive ((l ': ls) :: [[*]])) = If (Index a l == 'Nothing) (SuccMaybe (Index a (Recursive ls))) ('Just 0)
@@ -113,3 +117,79 @@ type family Take (n :: Nat) lst where
 
 --type instance RecIndex a ((l ': ls) :: [[*]]) = If (RecIndex a l == 'Nothing) (SuccMaybe (RecIndex a ls)) (Just 0)
 
+
+
+type family Last (lst :: [k]) :: k where
+    Last '[x]      = x
+    Last (x ': xs) = Last xs
+
+
+type family Init (lst :: [k]) :: [k] where
+    Init '[]       = '[]
+    Init '[x]      = '[]
+    Init (x ': xs) = x ': Init xs
+
+type family DropInit (lst :: [k]) :: [k] where
+    DropInit '[]       = '[]
+    DropInit '[x]      = '[x]
+    DropInit (x ': xs) = DropInit xs
+
+type family Replicate (n :: Nat) a where
+    Replicate 0 a = '[]
+    Replicate n a = a ': Replicate (n - 1) a
+
+
+type Zip a b = Zip2 a b
+
+type family Zip2 (l1 :: [*]) (l2 :: [*]) :: [*] where
+    Zip2 (x1 ': xs1) (x2 ': xs2) = (x1,x2) ': Zip2 xs1 xs2
+    Zip2 l1          l2          = '[]
+
+type family Zip3 (l1 :: [*]) (l2 :: [*]) (l3 :: [*]) :: [*] where
+    Zip3 (x1 ': xs1) (x2 ': xs2) (x3 ': xs3) = (x1,x2,x3) ': Zip3 xs1 xs2 xs3
+    Zip3 l1          l2          l3          = '[]
+
+type family Zip4 (l1 :: [*]) (l2 :: [*]) (l3 :: [*]) (l4 :: [*]) :: [*] where
+    Zip4 (x1 ': xs1) (x2 ': xs2) (x3 ': xs3) (x4 ': xs4) = (x1,x2,x3,x4) ': Zip4 xs1 xs2 xs3 xs4
+    Zip4 l1          l2          l3          l4          = '[]
+
+type family Zip5 (l1 :: [*]) (l2 :: [*]) (l3 :: [*]) (l4 :: [*]) (l5 :: [*]) :: [*] where
+    Zip5 (x1 ': xs1) (x2 ': xs2) (x3 ': xs3) (x4 ': xs4) (x5 ': xs5) = (x1,x2,x3,x4,x5) ': Zip5 xs1 xs2 xs3 xs4 xs5
+    Zip5 l1          l2          l3          l4          l5          = '[]
+
+
+type family Unzip2 (lst :: [*]) :: ([*], [*]) where
+    Unzip2 '[]              = '( '[], '[] )
+    Unzip2 ((x1,x2) ': lst) = PrependAll (x1,x2) (Unzip2 lst)
+
+--type family Unzip3 (lst :: [*]) :: ([*], [*], [*]) where
+--    Unzip3 '[]              = '( '[], '[] )
+--    Unzip3 ((x1,x2) ': lst) = PrependAll (x1,x2) (Unzip3 lst)
+
+
+type family PrependAll (els :: *) (lsts :: k) :: k
+type instance PrependAll (t1,t2) '(l1,l2) = '(t1 ': l1, t2 ': l2)
+type instance PrependAll (t1,t2,t3) '(l1,l2,l3) = '(t1 ': l1, t2 ': l2, t3 ': l3)
+type instance PrependAll (t1,t2,t3,t4) '(l1,l2,l3,l4) = '(t1 ': l1, t2 ': l2, t3 ': l3, t4 ': l4)
+type instance PrependAll (t1,t2,t3,t4,t5) '(l1,l2,l3,l4,l5) = '(t1 ': l1, t2 ': l2, t3 ': l3, t4 ': l4, t5 ': l5)
+
+
+type family Select (n :: Nat) (a :: *) :: *
+
+type instance Select 1 (t1,t2) = t1
+type instance Select 2 (t1,t2) = t2
+
+type instance Select 1 (t1,t2,t3) = t1
+type instance Select 2 (t1,t2,t3) = t2
+type instance Select 3 (t1,t2,t3) = t3
+
+type instance Select 1 (t1,t2,t3,t4) = t1
+type instance Select 2 (t1,t2,t3,t4) = t2
+type instance Select 3 (t1,t2,t3,t4) = t3
+type instance Select 4 (t1,t2,t3,t4) = t4
+
+type instance Select 1 (t1,t2,t3,t4,t5) = t1
+type instance Select 2 (t1,t2,t3,t4,t5) = t2
+type instance Select 3 (t1,t2,t3,t4,t5) = t3
+type instance Select 4 (t1,t2,t3,t4,t5) = t4
+type instance Select 5 (t1,t2,t3,t4,t5) = t5
