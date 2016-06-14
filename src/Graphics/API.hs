fc8acbadfa615b64cbcbd6db27888c118bfbaae0
Adam Szlachta <adam.szlachta@gmail.com>
2016-06-14 14:49:14 +0200

    Add convertible instances between all API shape layers

--- src/Graphics/API.hs
+++ src/Graphics/API.hs
@@ -1,20 +1,13 @@
 module Graphics.API where
 
--- import Graphics.API.Component      as X
--- import Graphics.API.Graphics       as X
--- import Graphics.API.Layer          as X
--- import Graphics.API.Material       as X
--- import Graphics.API.Shader         as X
--- import Graphics.API.Shape          as X
--- import Graphics.API.Transformation as X
-
 import           Control.Lens
-import           Data.Aeson   (FromJSON, ToJSON)
-import           Data.Binary  (Binary)
+import           Data.Aeson       (FromJSON, ToJSON)
+import           Data.Binary      (Binary)
+import           Data.Convert
 import           Data.Default
-import           Data.Map     (Map)
-import qualified Data.Map     as Map
-import           GHC.Generics (Generic)
+import           Data.Map         (Map)
+import qualified Data.Map         as Map
+import           GHC.Generics     (Generic)
 
 
 
@@ -26,15 +19,44 @@ import           GHC.Generics (Generic)
 data Point2 = Point2 Double Double
               deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
-instance Default Point2 where
-    def = Point2 def def
-
-
 data Point3 = Point3 Double Double Double
               deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
-instance Default Point3 where
-    def = Point3 def def def
+
+-- === Material === --
+
+-- TODO: expand possibilities
+data Material = SolidColor { _r :: Double
+                           , _g :: Double
+                           , _b :: Double
+                           , _a :: Double
+                           } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
+
+
+-- === Transformation === --
+
+-- TODO: matrix + methods
+data Transformation = Transformation { _scaleX :: Double
+                                     , _scaleY :: Double
+                                     , _dx     :: Double
+                                     , _dy     :: Double
+                                     , _angle  :: Double
+                                     , _refl   :: Bool
+                                     } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
+
+makeLenses ''Transformation
+
+scale :: Double -> Double ->  Transformation -> Transformation
+scale sx' sy' (Transformation sx sy dx dy a r) = Transformation (sx * sx') (sy * sy') dx dy a r
+
+translate :: Double -> Double -> Transformation -> Transformation
+translate dx' dy' (Transformation sx sy dx dy a r) = Transformation sx sy (dx + dx') (dy + dy') a r
+
+rotate :: Double -> Transformation -> Transformation
+rotate a' (Transformation sx sy dx dy a r) = Transformation sx sy dx dy (a + a') r
+
+reflect :: Transformation -> Transformation
+reflect (Transformation sx sy dx dy a r) = Transformation sx sy dx dy a (not r)
 
 
 -- === Attributes === --
@@ -51,88 +73,37 @@ type AttributeType = String
 
 -- lookup (AttrKey::AttrKey Attr.Point Attr.Color)
 
-
 data Attributes = Attributes (Map.Map AttributeType Double)
                   deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
-instance Default Attributes where
-    def = Attributes def
 
--- === Figure === --
+-- === Shader figures === --
 
+-- TODO: Convertibles
 data Figure = Square    { _s :: Double }
             | Rectangle { _w :: Double
                         , _h :: Double }
             | Circle    { _d :: Double }
             deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
+-- TODO: remove position
+data Primitive = Primitive { _figure     :: Figure
+                           , _position   :: Point2
+                           , _attributes :: Attributes
+                           } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
--- === Primitive === --
-
-data Primitive = Primitive Figure Point2 Attributes
-                 deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
-
-
--- === Shape === --
-
-data Shape = Single    Primitive
+-- TODO: add transformation (using auxiliary data type)
+-- TODO: consider lists of merge and intersect
+data Shape = Shape     Primitive
            | Merge     Shape Shape
            | Subtract  Shape Shape
            | Intersect Shape Shape
            deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
-
--- === Surface === --
-
 data Surface = ShapeSurface Shape
              | PolygonSurface
              | NumbsSurface
              deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
-
-
--- === Transformation === --
-
--- TODO: matrix + methods
-data Transformation = Transformation { _scaleX :: Double
-                                     , _scaleY :: Double
-                                     , _dx     :: Double
-                                     , _dy     :: Double
-                                     , _angle  :: Double
-                                     , _refl   :: Bool
-                                     } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
-
-instance Default Transformation where
-    def = Transformation 1.0 1.0 0.0 0.0 0.0 False
-
-makeLenses ''Transformation
-
-scale :: Transformation -> Double -> Double -> Transformation
-scale (Transformation sx sy dx dy a r) sx' sy' = Transformation (sx * sx') (sy * sy') dx dy a r
-
-translate :: Transformation -> Double -> Double -> Transformation
-translate (Transformation sx sy dx dy a r) dx' dy' = Transformation sx sy (dx + dx') (dy + dy') a r
-
-rotate :: Transformation -> Double -> Transformation
-rotate (Transformation sx sy dx dy a r) a' = Transformation sx sy dx dy (a + a') r
-
-reflect :: Transformation -> Transformation
-reflect (Transformation sx sy dx dy a r) = Transformation sx sy dx dy a (not r)
-
-
--- === Material === --
-
--- TODO: expand possibilities
-data Material = SolidColor { _r :: Double
-                           , _g :: Double
-                           , _b :: Double
-                           , _a :: Double
-                           } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
-
-instance Default Material where
-    def = SolidColor 1.0 1.0 1.0 1.0
-
-
--- === Geometry === --
 
 data Geometry = Geometry { _content        :: GeoComponent
                          , _transformation :: Transformation
@@ -144,14 +115,91 @@ data GeoComponent = GeoElem  [Surface]
                   deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
 
--- === Layer === --
+-- === Multiple shaders layers === --
 
 data Layer = Layer { _geometry        :: Geometry
                    , _transformations :: [Transformation]
                    } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
 
-
--- === Graphics === --
-
 data Graphics = Graphics { _graphics :: [Layer]
                          } deriving (Show, Eq, Generic, Binary, ToJSON, FromJSON)
+
+
+-------------------------------
+-- === Default instances === --
+-------------------------------
+
+instance Default Point2 where
+    def = Point2 def def
+
+instance Default Point3 where
+    def = Point3 def def def
+
+instance Default Material where
+    def = SolidColor 1.0 1.0 1.0 1.0
+
+instance Default Transformation where
+    def = Transformation 1.0 1.0 0.0 0.0 0.0 False
+
+instance Default Attributes where
+    def = Attributes def
+
+-----------------------------------
+-- === Convertible instances === --
+-----------------------------------
+
+figureToPrimitive :: Figure -> Primitive
+figureToPrimitive figure = Primitive figure def def
+
+primitiveToShape :: Primitive -> Shape
+primitiveToShape = Shape
+
+shapeToSurface :: Shape -> Surface
+shapeToSurface = ShapeSurface
+
+surfaceToGeoComponent :: Surface -> GeoComponent
+surfaceToGeoComponent = GeoElem . pure
+
+geoComponentToGeometry :: GeoComponent -> Geometry
+geoComponentToGeometry geoComponent = Geometry geoComponent def (Just def)
+
+geometryToLayer :: Geometry -> Layer
+geometryToLayer geometry = Layer geometry [def]
+
+layerToGraphics :: Layer -> Graphics
+layerToGraphics = Graphics . pure
+
+instance Convertible Layer        Graphics     where convert =           layerToGraphics
+instance Convertible Geometry     Graphics     where convert = convert . geometryToLayer
+instance Convertible GeoComponent Graphics     where convert = convert . geoComponentToGeometry
+instance Convertible Surface      Graphics     where convert = convert . surfaceToGeoComponent
+instance Convertible Shape        Graphics     where convert = convert . shapeToSurface
+instance Convertible Primitive    Graphics     where convert = convert . primitiveToShape
+instance Convertible Figure       Graphics     where convert = convert . figureToPrimitive
+
+instance Convertible Geometry     Layer        where convert =           geometryToLayer
+instance Convertible GeoComponent Layer        where convert = convert . geoComponentToGeometry
+instance Convertible Surface      Layer        where convert = convert . surfaceToGeoComponent
+instance Convertible Shape        Layer        where convert = convert . shapeToSurface
+instance Convertible Primitive    Layer        where convert = convert . primitiveToShape
+instance Convertible Figure       Layer        where convert = convert . figureToPrimitive
+
+instance Convertible GeoComponent Geometry     where convert =           geoComponentToGeometry
+instance Convertible Surface      Geometry     where convert = convert . surfaceToGeoComponent
+instance Convertible Shape        Geometry     where convert = convert . shapeToSurface
+instance Convertible Primitive    Geometry     where convert = convert . primitiveToShape
+instance Convertible Figure       Geometry     where convert = convert . figureToPrimitive
+
+instance Convertible Surface      GeoComponent where convert =           surfaceToGeoComponent
+instance Convertible Shape        GeoComponent where convert = convert . shapeToSurface
+instance Convertible Primitive    GeoComponent where convert = convert . primitiveToShape
+instance Convertible Figure       GeoComponent where convert = convert . figureToPrimitive
+
+instance Convertible Shape        Surface      where convert =           shapeToSurface
+instance Convertible Primitive    Surface      where convert = convert . primitiveToShape
+instance Convertible Figure       Surface      where convert = convert . figureToPrimitive
+
+instance Convertible Primitive    Shape        where convert =           primitiveToShape
+instance Convertible Figure       Shape        where convert = convert . figureToPrimitive
+
+instance Convertible Figure       Primitive    where convert =           figureToPrimitive
