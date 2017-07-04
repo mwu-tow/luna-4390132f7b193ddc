fe6836188d4463b1087fbd6f4d3daf01804fb8df
Sylwia Brodacka <s.brodacka@gmail.com>
2017-07-04 11:54:51 +0200

    refactoring and darwin installator

--- src/Luna/Manager/Command/Install.hs
+++ src/Luna/Manager/Command/Install.hs
@@ -18,15 +18,17 @@ import Control.Lens.Aeson
 import Control.Monad.Raise
 import Control.Monad.State.Layered
 
+import Data.Maybe (listToMaybe)
 import qualified Data.Map as Map
+import qualified Data.Text as Text
+
 
 import qualified Data.Yaml as Yaml
 
-import Filesystem.Path.CurrentOS (FilePath, (</>), encodeString, toText, fromText, basename)
+import Filesystem.Path.CurrentOS (FilePath, (</>), encodeString, toText, fromText, basename, hasExtension)
 import Shelly.Lifted (toTextIgnore)
 import qualified Shelly.Lifted as Shelly
 import Luna.Manager.Archive
-import Luna.Manager.Component.WindowsInstallationUtils
 
 ---------------------------------
 -- === Installation config === --
@@ -48,7 +50,7 @@ instance Monad m => MonadHostConfig InstallConfig 'Linux arch m where
     defaultHostConfig = return $ InstallConfig
         { _execName        = "luna-studio"
         , _defaultConfPath = "~/.luna"
-        , _defaultBinPath  = "~/.luna-bin" --TODO jak tylko bedzie poprawne expandowanie tyldy to przywrócic stara wersje
+        , _defaultBinPath  = "~/.luna-bin"
         , _localName       = "local"
         }
 
@@ -60,7 +62,7 @@ instance Monad m => MonadHostConfig InstallConfig 'Darwin arch m where
 instance Monad m => MonadHostConfig InstallConfig 'Windows arch m where
     defaultHostConfig = reconfig <$> defaultHostConfigFor @Linux where
         reconfig cfg = cfg & execName       .~ "LunaStudio"
-                           & defaultBinPath .~ "C:\\ProgramFiles"
+                           & defaultBinPath .~ "C:\\Program Files"
 
 
 
@@ -76,10 +78,87 @@ makeLenses ''UnresolvedDepsError
 instance Exception UnresolvedDepsError where
     displayException err = "Following dependencies were unable to be resolved: " <> show (showPretty <$> unwrap err)
 
+data ExecutableNotFound = ExecutableNotFound deriving (Show)
+instance Exception ExecutableNotFound where
+    displayException _ = "Couldn't find executable"
+
+executableNotFound :: SomeException
+executableNotFound = toException ExecutableNotFound
 
 -- === Running === --
 
 type MonadInstall m = (MonadStates '[EnvConfig, InstallConfig, RepoConfig] m, MonadNetwork m)
+
+prepareInstallPath :: MonadInstall m => Text -> Text -> Text -> m FilePath
+prepareInstallPath appPath appName appVersion = do
+    case currentHost of
+        Linux   -> do
+            installPath <- expand $ (fromText appPath) </> (fromText appName) </> (fromText appVersion)
+            return installPath
+        Windows -> do
+            installPath <- expand $ (fromText appPath) </> (fromText appName) </> (fromText appVersion)
+            return installPath
+        Darwin  -> do
+            exec <- view execName <$> get @InstallConfig
+            installPath <- expand $ (fromText appPath) </> (fromText (Text.append exec ".app")) </> (fromText "Contents") </> (fromText "Resources") </> (fromText appVersion)
+            return installPath
+
+downloadAndUnpack :: MonadInstall m => URIPath -> FilePath -> m ()
+downloadAndUnpack pkgPath installPath = do
+    Shelly.shelly $ Shelly.mkdir_p installPath
+    tmp <- getTmpPath
+    pkg <- downloadWithProgressBar pkgPath tmp
+    case currentHost of
+        Linux -> do
+            Shelly.shelly $ Shelly.cp pkg installPath
+        Darwin -> do
+            Shelly.shelly $ unpackTarGzUnix pkg
+            let packageFolder = basename pkg
+                unzippedPackagePath = tmp </> packageFolder
+            Shelly.shelly $ copyDir unzippedPackagePath installPath
+        Windows -> do
+            unzipFileWindows pkg
+            let packageFolder = basename pkg
+                unzippedPackagePath = tmp </> packageFolder
+            Shelly.shelly $ copyDir unzippedPackagePath installPath
+
+
+
+postInstallation :: MonadInstall m => FilePath -> Text -> m()
+postInstallation installPath appPath = do
+    home <- getHomePath
+    case currentHost of
+        Linux -> do
+            execList <- Shelly.shelly $  Shelly.findWhen (pure . Shelly.hasExt "AppImage") installPath --może inny sposób na przekazywanie ścieżki do executabla ??
+            appimage <- tryJust executableNotFound $ listToMaybe execList
+            makeExecutable appimage
+            exec <- view execName <$> get @InstallConfig
+            currentAppimage <- expand $ (fromText appPath) </> (fromText exec)
+            let localBinDir = home </> ".local/bin" -- TODO: moe to state and use the same code for windows
+                localBin = home </> ".local/bin" </> (fromText exec)
+            Shelly.shelly $ Shelly.mkdir_p localBinDir
+            createSymLink appimage currentAppimage
+            createSymLink currentAppimage localBin
+            shell <- checkShell
+            exportPath localBinDir shell -- rename export because it is not export to env
+        Darwin  -> do
+            exec          <- view execName <$> get @InstallConfig
+            let resourcesBin  = installPath </> (fromText exec)
+                localBinDir   = home </> ".local/bin"
+                localBin      = home </> ".local/bin" </> (fromText exec)
+            currentBinDir <- expand $ (fromText appPath) </> (fromText (Text.append exec ".app")) </> (fromText "Contents") </> (fromText "MacOS")
+            currentBin    <- expand $ (fromText appPath) </> (fromText (Text.append exec ".app")) </> (fromText "Contents") </> (fromText "MacOS") </> (fromText exec)
+
+            Shelly.shelly $ Shelly.mkdir_p currentBinDir
+            createSymLink resourcesBin currentBin
+            createSymLink currentBin localBin
+
+        Windows -> do
+            let services = installPath </> (fromText "services") --sama względna ścieżka do serwisów też do tego samego statea windows installation utils
+            Shelly.shelly $ runServicesWindows services
+            --copy shortcut to menu programs
+
+
 
 runInstaller :: MonadInstall m => InstallOpts -> m ()
 runInstaller opts = do
@@ -105,44 +184,13 @@ runInstaller opts = do
         $ question "Select installation path" plainTextReader
         & defArg .~ Just (toTextIgnore (installConfig ^. defaultBinPath)) --TODO uzyć toText i złapać tryRight'
 
-    -- print $ "TODO: Install the app (with progress bar): "  <> appName
 
     let pkgPath = appPkgDesc ^. path
-    home <- getHomePath
-    print $ show home
+    installPath <- prepareInstallPath appPath appName appVersion
+    downloadAndUnpack pkgPath installPath
+    postInstallation installPath appPath
 
-    installPath <- expand $ (fromText appPath) </> (fromText appName) </> (fromText appVersion)
-    createDirIfMissingTrue installPath
-    print $ show installPath
-    case currentHost of
-        Linux   -> do
-            appimage <- downloadWithProgressBar pkgPath installPath
-            makeExecutable appimage
-            exec <- view execName <$> get @InstallConfig
-            currentAppimage <- expand $ (fromText appPath) </> (fromText exec)
-            let localBinDir = home </> ".local/bin"
-                localBin = home </> ".local/bin" </> (fromText exec)
-            createDirIfMissingTrue localBinDir
-            createSymLink appimage currentAppimage
-            createSymLink currentAppimage localBin
-            shell <- checkShell
-            exportPath localBinDir shell
-        Darwin  -> return ()
-        Windows -> do
-            tmp <- getTmpPath
-            zippedPackage <- downloadWithProgressBar pkgPath tmp
-            let scriptPath = "https://s3-us-west-2.amazonaws.com/packages-luna/windows/j_unzip.vbs" -- do osobnego statea dla windows installation utils?
-            script <- downloadFromURL scriptPath
 
-            print zippedPackage
-            print script
-            unpacked <- Shelly.shelly $ unzipFileWindows script zippedPackage tmp
-            let packageFolder = basename zippedPackage
-                unzippedPackagePath = tmp </> packageFolder
-            Shelly.shelly $ Shelly.cp_r unzippedPackagePath installPath
-            let services = installPath </> packageFolder </> (fromText "services") --sama względna ścieżka do serwisów też do tego samego statea windows installation utils
-            Shelly.shelly $ runServicesWindows services
-            return ()
 
 
     -- print $ "TODO: Install the libs (each with separate progress bar): " <> show libsToInstall -- w ogóle nie supportujemy przeciez instalowania osobnych komponentów i libów
--- src/Luna/Manager/System/Env.hs
+++ src/Luna/Manager/System/Env.hs
@@ -8,7 +8,8 @@ import Luna.Manager.System.Host
 import Filesystem.Path.CurrentOS
 import Control.Monad.State.Layered
 import qualified System.Directory as System
-
+import Control.Monad.Raise
+import qualified Shelly.Lifted as Shelly
 
 --------------------------
 -- === EnvConfig === --
@@ -33,23 +34,28 @@ getCurrentPath = do
     current <- liftIO $ System.getCurrentDirectory
     return $ fromText $ convert current
 
-getTmpPath, getDownloadPath :: MonadGetter EnvConfig m => m FilePath
-getTmpPath      = view localTempPath <$> get @EnvConfig
+getTmpPath, getDownloadPath :: (MonadIO m, MonadGetter EnvConfig m) => m FilePath
+getTmpPath      = do
+    tmp <- view localTempPath <$> get @EnvConfig
+    Shelly.shelly $ Shelly.mkdir_p tmp
+    return tmp
 getDownloadPath = getTmpPath
 
 
 setTmpCwd :: (MonadGetter EnvConfig m, MonadIO m) => m ()
 setTmpCwd = liftIO . System.setCurrentDirectory . encodeString =<< getTmpPath
 
-createDir :: MonadIO m => FilePath -> m ()
-createDir path = liftIO $ System.createDirectory $ encodeString path
-
 createSymLink :: MonadIO m => FilePath -> FilePath -> m ()
 createSymLink src dst = liftIO $ System.createFileLink (encodeString src) (encodeString dst)
 
-createDirIfMissingTrue :: MonadIO m => FilePath-> m ()
-createDirIfMissingTrue path = liftIO $ System.createDirectoryIfMissing True $ encodeString path
+
+
+copyDir :: Shelly.MonadSh m => FilePath -> FilePath -> m ()-- copy the content of the source directory
+copyDir src dst = do
+    listedDirectory <- Shelly.ls src
+    mapM_ (flip Shelly.cp_r dst) listedDirectory
 
 -- === Instances === --
 instance {-# OVERLAPPABLE #-} MonadIO m => MonadHostConfig EnvConfig sys arch m where
-    defaultHostConfig = EnvConfig <$> (decodeString <$> liftIO System.getTemporaryDirectory)
+    defaultHostConfig = EnvConfig <$> tmp where
+        tmp =  (</> "tmp/luna") <$> decodeString <$> liftIO System.getTemporaryDirectory
