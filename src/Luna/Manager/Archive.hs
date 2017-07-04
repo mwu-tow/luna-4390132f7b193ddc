571152366f4e82e2943ecd51bd4b60462ddeecbd
Sylwia Brodacka <s.brodacka@gmail.com>
2017-07-04 11:53:49 +0200

    refactor unpacking

--- src/Luna/Manager/Archive.hs
+++ src/Luna/Manager/Archive.hs
@@ -3,21 +3,33 @@
 module Luna.Manager.Archive where
 
 import Luna.Manager.Shell.Commands
+import Luna.Manager.Network
 
 import Prologue hiding (FilePath)
 
-import Filesystem.Path.CurrentOS (FilePath, (</>), encodeString, toText, fromText, filename)
+import Filesystem.Path.CurrentOS (FilePath, (</>), encodeString, toText, fromText, filename, directory)
 import qualified Shelly.Lifted as Shelly
 import           Shelly.Lifted (MonadSh)
 import qualified Data.Text as Text
 default (Text.Text)
 
-unpackTarGzUnix :: Shelly.MonadSh m => FilePath -> FilePath -> m ()
-unpackTarGzUnix file dst = do
-    Shelly.cd dst
-    Shelly.cmd  "tar" "-xf" file "--strip=1"
 
-unzipFileWindows :: Shelly.MonadSh m => FilePath -> FilePath -> FilePath -> m ()
-unzipFileWindows script zipFile dst = do
-  Shelly.cd dst
-  Shelly.cmd "cscript" (filename script) (filename zipFile)
+
+-- TODO: wspolny upack
+unpackTarGzUnix :: Shelly.MonadSh m => FilePath -> m ()
+unpackTarGzUnix file = do
+    let dir = directory file
+    Shelly.cd dir
+    Shelly.cmd  "tar" "-xvpzf" file
+
+-- TODO: download unzipper if missing
+unzipFileWindows :: (MonadIO m, MonadNetwork m)=> FilePath -> m ()
+unzipFileWindows zipFile = do
+    let scriptPath = "https://s3-us-west-2.amazonaws.com/packages-luna/windows/j_unzip.vbs"
+    --sprawdź czy jest na dysku, shelly.find, skrypt i plik musza byc w tym samym directory
+
+    script <- downloadFromURL scriptPath
+    let dir = directory zipFile
+    Shelly.shelly $ Shelly.cp script dir
+    Shelly.shelly $ Shelly.cd dir
+    Shelly.shelly $ Shelly.cmd "cscript" (filename script) (filename zipFile)
