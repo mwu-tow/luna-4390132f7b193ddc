5a7330c73ebe6610547c4422403802450933b9ee
Jan Kłosowski <janklosowski@gmail.com>
2017-07-14 16:51:06 +0200

    Refactoring of SVG icons #66

--- luna-studio/node-editor/src/NodeEditor/React/View/ExpressionNode.hs
+++ luna-studio/node-editor/src/NodeEditor/React/View/ExpressionNode.hs
@@ -92,8 +92,8 @@ nodeName_ ref nl nodeName mayVisualizationVisible mayS = do
                     , onDoubleClick $ \e _ -> [stopPropagation e]
                     , onClick       $ \_ _ -> dispatch ref $ UI.VisualizationEvent $ Visualization.ToggleVisualizations nl
                     ] $ if isVisualization
-                        then path_ [ "d" $= "M12 4.5c-5 0-9.3 3-11 7.5 1.7 4.4 6 7.5 11 7.5s9.3-3 11-7.5c-1.7-4.4-6-7.5-11-7.5zM12 17c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5zm0-8c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3z" ] mempty
-                        else path_ [ "d" $= "M12 7c2.8 0 5 2.2 5 5 0 .7 0 1.3-.4 1.8l3 3c1.5-1.3 2.7-3 3.4-4.8-1.7-4.4-6-7.5-11-7.5-1.4 0-2.7.3-4 .7l2.2 2.2C10.7 7 11.4 7 12 7zM2 4.3l2.3 2.2.4.5C3 8.3 1.7 10 1 12c1.7 4.4 6 7.5 11 7.5 1.6 0 3-.3 4.4-.8l.4.4 3 3 1.2-1L3.3 3 2 4.3zm5.5 5.5L9 11.4v.6c0 1.7 1.3 3 3 3h.7l1.5 1.5c-.7.3-1.4.5-2.2.5-2.8 0-5-2.2-5-5 0-.8.2-1.5.5-2.2zm4.3-.8l3.2 3.2V12c0-1.6-1.3-3-3-3h-.2z" ] mempty
+                        then path_ [ "d" $= Style.iconEye         ] mempty
+                        else path_ [ "d" $= Style.iconEyeDisabled ] mempty
 
 nodeExpression_ :: Ref App -> NodeLoc -> Text -> Maybe Searcher -> ReactElementM ViewEventHandler ()
 nodeExpression_ ref nl expr mayS = do
