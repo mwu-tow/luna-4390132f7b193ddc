67ddf4a244bdc236af4d952b4f375a140e9bbfe3
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2017-07-17 17:11:22 +0200

    Fix arguments for InterpreterEnv

--- libs/luna-empire/test/EmpireUtils.hs
+++ libs/luna-empire/test/EmpireUtils.hs
@@ -79,7 +79,7 @@ graphIDs loc = do
     return ids
 
 extractGraph :: InterpreterEnv -> ClsGraph
-extractGraph (InterpreterEnv _ _ _ g _ _ _) = g
+extractGraph (InterpreterEnv _ _ _ _ g _ _ _) = g
 
 withResult :: a -> (a -> IO b) -> IO b
 withResult res act = act res
