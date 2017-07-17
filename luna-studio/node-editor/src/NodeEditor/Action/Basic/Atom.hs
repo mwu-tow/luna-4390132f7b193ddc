cdf25a034b9974cb10dbb04cf6ccc3ad6e542f31
LJK <leszek.kania@luna-lang.org>
2017-07-17 17:31:29 +0200

    Request node searcher data only if there is no data in scope. #106

--- luna-studio/node-editor/src/NodeEditor/Action/Basic/Atom.hs
+++ luna-studio/node-editor/src/NodeEditor/Action/Basic/Atom.hs
@@ -7,7 +7,7 @@ import           NodeEditor.Action.Batch                (searchNodes)
 import           NodeEditor.Action.Camera.Persistence   (saveCamera)
 import           NodeEditor.Action.Command              (Command)
 import           NodeEditor.Action.State.NodeEditor     (resetGraph)
-import           NodeEditor.Batch.Workspace             (currentLocation)
+import           NodeEditor.Batch.Workspace             (currentLocation, nodeSearcherData)
 import qualified NodeEditor.Batch.Workspace             as Workspace
 import           NodeEditor.State.Global                (State, workspace)
 
@@ -16,10 +16,11 @@ setFile :: FilePath -> Command State ()
 setFile path = do
     saveCamera
     mayCurrentFilePath <- preuse $ workspace . traverse . currentLocation . filePath
+    mayNsData <- preuse $ workspace . traverse . nodeSearcherData
     when (Just path /= mayCurrentFilePath) $ do
         let newWorkspace = Workspace.mk path
         workspace ?= newWorkspace
-        searchNodes
+        maybe searchNodes (workspace . _Just . nodeSearcherData .=) mayNsData
         loadGraph $ newWorkspace ^. currentLocation
 
 unsetFile :: Command State ()
