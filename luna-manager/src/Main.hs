12ffc9fe3df56ce540cb1bc26eb04a665e15dc7c
Sylwia <s.brodacka@gmail.com>
2017-08-09 12:55:43 +0200

    handle sigTerm

--- luna-manager/src/Main.hs
+++ luna-manager/src/Main.hs
@@ -1,14 +1,39 @@
 module Main where
 
-import Prologue
+import Prologue hiding (FilePath)
 import Control.Monad.Raise
 import Luna.Manager.Command.Options (evalOptionsParserT)
 import Luna.Manager.Command         (chooseCommand)
 
+import Filesystem.Path.CurrentOS (encodeString, FilePath)
+import System.Directory (removeDirectoryRecursive)
+import Luna.Manager.System.Env (getTmpPath, EnvConfig)
+import Luna.Manager.System.Host
+import Control.Monad.Raise
+import Control.Monad.State.Layered
+import System.Signal
+import Control.Concurrent (threadDelay, ThreadId, killThread, myThreadId)
 
+
+evalGetTmp :: (MonadIO m) => m FilePath
+evalGetTmp = evalDefHostConfigs @'[EnvConfig] $ getTmpPath
+
+cleanUp :: MonadIO m => FilePath -> m ()
+cleanUp tmp = liftIO $ removeDirectoryRecursive $ encodeString tmp
+
+termHandler :: FilePath -> ThreadId -> Signal -> IO ()
+termHandler tmp tId s = do
+    putStrLn $ "Exit with code: " <> (show s)
+    cleanUp tmp
+    killThread tId
 
 main :: IO ()
-main = handleAll handleTopLvlError $ evalOptionsParserT chooseCommand 
+main = do
+    tmp <- evalGetTmp
+    threadId <- myThreadId
+    installHandler sigTERM (termHandler tmp threadId)
+    installHandler sigINT (termHandler tmp threadId)
+    handleAll handleTopLvlError $ evalOptionsParserT chooseCommand
 
 handleTopLvlError :: MonadIO m => SomeException -> m ()
 handleTopLvlError e = do
