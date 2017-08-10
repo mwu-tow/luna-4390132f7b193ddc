3943232d8a3a2d2c012ba3b6ec1176bca573bb33
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-08-10 14:58:08 +0200

    Overall progress

--- luna-manager/src/Luna/Manager/Command/Develop.hs
+++ luna-manager/src/Luna/Manager/Command/Develop.hs
@@ -6,10 +6,17 @@ import Luna.Manager.Network
 import Luna.Manager.System.Env
 import Control.Monad.Raise
 import Control.Monad.State.Layered
-import Luna.Manager.Shell.Shelly
+import Luna.Manager.Shell.Shelly   (MonadSh)
+import Luna.Manager.System.Env     (move)
+
+import qualified Luna.Manager.Shell.Shelly as Shelly
+import qualified Luna.Manager.Archive      as Archive
+
 
 run :: (MonadIO m, MonadException SomeException m, MonadGetter EnvConfig m, MonadSh m) => DevelopOpts -> m ()
 run opts = do
+    Shelly.mkdir_p "luna"
     stackArchive <- downloadWithProgressBar "https://github.com/commercialhaskell/stack/releases/download/v1.5.1/stack-1.5.1-linux-x86_64-static.tar.gz"
-
+    stackPath    <- Archive.unpack stackArchive
+    print stackPath
     print "hello"
