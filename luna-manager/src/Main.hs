9e9ddd1ff0ae6c716f979fe54f4cab0cdf61fde3
unknown <s.brodacka@gmail.com>
2017-08-10 08:18:53 +0100

    properly handle signals on windows

--- luna-manager/src/Main.hs
+++ luna-manager/src/Main.hs
@@ -5,35 +5,53 @@ import Control.Monad.Raise
 import Luna.Manager.Command.Options (evalOptionsParserT)
 import Luna.Manager.Command         (chooseCommand)
 
-import Filesystem.Path.CurrentOS (encodeString, FilePath)
+import Filesystem.Path.CurrentOS (encodeString, decodeString, FilePath)
 import System.Directory (removeDirectoryRecursive)
 import Luna.Manager.System.Env (getTmpPath, EnvConfig)
 import Luna.Manager.System.Host
 import Control.Monad.Raise
 import Control.Monad.State.Layered
-import System.Signal
+import GHC.ConsoleHandler
 import Control.Concurrent (threadDelay, ThreadId, killThread, myThreadId)
-
+import System.Process.Typed
+import qualified Control.Exception.Safe as Exception
+import qualified System.Signal as Signal
 
 evalGetTmp :: (MonadIO m) => m FilePath
 evalGetTmp = evalDefHostConfigs @'[EnvConfig] $ getTmpPath
 
 cleanUp :: MonadIO m => FilePath -> m ()
-cleanUp tmp = liftIO $ removeDirectoryRecursive $ encodeString tmp
+cleanUp tmp = do
+    print "cleanUp"
+    runProcess_ $ shell ("RD /S /Q " ++ (encodeString tmp))
+    -- liftIO $ removeDirectoryRecursive $ encodeString tmp
 
-termHandler :: FilePath -> ThreadId -> Signal -> IO ()
-termHandler tmp tId s = do
-    putStrLn $ "Exit with code: " <> (show s)
-    cleanUp tmp
+termHandler :: ThreadId -> Signal.Signal -> IO ()
+termHandler tId s = do
+
+    -- cleanUp tmp
     killThread tId
+
+termHandler2 :: ThreadId -> Handler
+termHandler2 tid  = Catch $ \_ -> do
+    killThread tid
 
 main :: IO ()
 main = do
     tmp <- evalGetTmp
     threadId <- myThreadId
-    installHandler sigTERM (termHandler tmp threadId)
-    installHandler sigINT (termHandler tmp threadId)
-    handleAll handleTopLvlError $ evalOptionsParserT chooseCommand
+    print threadId
+    case currentHost of
+        Windows -> void $ installHandler (termHandler2 threadId )
+        Linux -> do
+            Signal.installHandler Signal.sigTERM (termHandler threadId)
+            Signal.installHandler Signal.sigINT (termHandler threadId)
+        Darwin -> do
+            Signal.installHandler Signal.sigTERM (termHandler threadId)
+            Signal.installHandler Signal.sigINT (termHandler threadId)
+    evalOptionsParserT chooseCommand `Exception.finally` (cleanUp tmp)
+
+instance Exception e => MonadException e IO where raise = Exception.throwM
 
 handleTopLvlError :: MonadIO m => SomeException -> m ()
 handleTopLvlError e = do
