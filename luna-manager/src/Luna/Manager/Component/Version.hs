0e86aca633af8bb768d834e1c932d5bbdcd69f37
Sylwia <s.brodacka@gmail.com>
2017-08-24 16:45:07 +0200

    add Nightly to package Version type

--- luna-manager/src/Luna/Manager/Component/Version.hs
+++ luna-manager/src/Luna/Manager/Component/Version.hs
@@ -22,43 +22,56 @@ data VersionTag = Alpha
                 | RC !Word64
                 deriving (Generic, Show, Eq, Ord)
 
-data Version = Version { _major :: !Word64
-                       , _minor :: !Word64
-                       , _build :: !Word64
-                       , _tag   :: !(Maybe VersionTag)
+data Nightly = Nightly !Word64 deriving (Generic, Show, Eq, Ord)
+
+data Version = Version { _major   :: !Word64
+                       , _minor   :: !Word64
+                       , _build   :: !Word64
+                       , _tag     :: !(Maybe VersionTag)
+                       , _nightly :: !(Maybe Nightly)
                        } deriving (Generic, Show, Eq, Ord)
 
 makeLenses ''Version
+makeLenses ''Nightly
 makeLenses ''VersionTag
-
 
 -- === Instances === --
 
 instance Pretty VersionTag where
     showPretty = \case
-        Alpha -> "alpha"
-        Beta  -> "beta"
-        RC i  -> "rc" <> convert (show i)
+        Alpha   -> "alpha"
+        Beta    -> "beta"
+        RC i    -> "rc" <> convert (show i)
     readPretty = \case
-        "alpha" -> Right Alpha
-        "beta"  -> Right Beta
-        s       -> case Text.take 2 s of
+        "alpha"   -> Right Alpha
+        "beta"    -> Right Beta
+        s         -> case Text.take 2 s of
             "rc" -> RC <$> mapLeft (const "Conversion error") (tryReads @String $ Text.drop 2 s)
             _    -> Left "Incorrect version tag format"
 
+instance Pretty Nightly where
+    showPretty (Nightly i) = "nightly" <> convert (show i)
+    readPretty s = case Text.take 7 s of
+        "nightly" -> Nightly <$> mapLeft (const "Conversion error") (tryReads @String $ Text.drop 7 s)
+        _ -> Left "Incorrect version tag format"
+
 instance Pretty Version where
-    showPretty (Version major minor build tag) = intercalate "." (map (convert . show) [major, minor, build])
-                                              <> maybe "" (("." <>) . showPretty) tag
+    showPretty (Version major minor build tag nightly) = intercalate "." (map (convert . show) [major, minor, build])
+                                              <> maybe "" (("." <>) . showPretty) tag <> maybe "" (("." <>) . showPretty) nightly
     readPretty t = case Text.splitOn "." t of
-        [ma, mi, b, t] -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> (Just <$> mapLeft convert (readPretty t))
-        [ma, mi, b]    -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> pure Nothing
-        _              -> Left "Incorrect version format"
+        [ma, mi, b, t, n] -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> (Just <$> mapLeft convert (readPretty t)) <*> (Just <$> mapLeft convert (readPretty n))
+        [ma, mi, b, n]    -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> pure Nothing <*> (Just <$> mapLeft convert (readPretty n))
+        [ma, mi, b, t]    -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> (Just <$> mapLeft convert (readPretty t)) <*> pure Nothing
+        [ma, mi, b]       -> cerr $ Version <$> tryReads ma <*> tryReads mi <*> tryReads b <*> pure Nothing <*> pure Nothing
+        _                 -> Left "Incorrect version format"
         where cerr = mapLeft (const "Conversion error")
 
 -- JSON
 instance ToJSON      Version    where toEncoding  = JSON.toEncoding . showPretty; toJSON = JSON.toJSON . showPretty
+instance ToJSON      Nightly    where toEncoding  = lensJSONToEncoding; toJSON = lensJSONToJSON
 instance ToJSON      VersionTag where toEncoding  = lensJSONToEncoding; toJSON = lensJSONToJSON
 instance FromJSON    Version    where parseJSON   = either (fail . convert) return . readPretty <=< parseJSON
+instance FromJSON    Nightly    where parseJSON   = lensJSONParse
 instance FromJSON    VersionTag where parseJSON   = lensJSONParse
 instance FromJSONKey Version    where fromJSONKey = JSON.FromJSONKeyTextParser $ either (fail . convert) return . readPretty
 instance ToJSONKey   Version    where
