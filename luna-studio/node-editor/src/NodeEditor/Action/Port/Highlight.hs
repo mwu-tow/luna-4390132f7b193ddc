ba1d750766a7f1945ff28e406ce82718d45a5293
LJK <leszek.kania@luna-lang.org>
2017-09-14 17:17:19 +0200

    Fix output sidebar. #200

--- luna-studio/node-editor/src/NodeEditor/Action/Port/Highlight.hs
+++ luna-studio/node-editor/src/NodeEditor/Action/Port/Highlight.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE RankNTypes       #-}
 module NodeEditor.Action.Port.Highlight
     ( handleMouseEnter
     , handleMouseLeave
@@ -11,44 +12,47 @@ import           LunaStudio.Data.Port                       (AnyPortId (InPortId
 import           LunaStudio.Data.PortRef                    (AnyPortRef (..), nodeLoc, portId)
 import           NodeEditor.Action.State.Action             (runningActions)
 import           NodeEditor.Action.State.Model              (calculatePortMode)
-import           NodeEditor.Action.State.NodeEditor         (getExpressionNode, modifyExpressionNode)
-import           NodeEditor.React.Model.Node.ExpressionNode (argConstructorMode, hasPort, inPortAt, outPortAt)
-import           NodeEditor.React.Model.Port                (Mode (..), mode)
+import           NodeEditor.Action.State.NodeEditor         (getExpressionNode, modifyExpressionNode, modifyOutputNode)
+import           NodeEditor.React.Model.Node.ExpressionNode (argConstructorMode, hasPort)
+import           NodeEditor.React.Model.IsNode              (inPortAt, outPortAt, HasPorts)
+import           NodeEditor.React.Model.Port                (Mode (..), mode, AnyPortId)
 import           NodeEditor.State.Action                    (actionsBlockingPortHighlight)
 import           NodeEditor.State.Global                    (State)
 
+portModeLens :: HasPorts n => AnyPortId -> Traversal' n Mode
+portModeLens = \case
+    OutPortId' outpid -> outPortAt outpid . mode
+    InPortId'  inpid  -> inPortAt  inpid  . mode
 
 handleMouseEnter :: AnyPortRef -> Command State ()
 handleMouseEnter portRef = do
     actions <- Set.fromList <$> runningActions
-    let notBlocked = Set.null (Set.intersection actions actionsBlockingPortHighlight)
-    when notBlocked $ modifyExpressionNode (portRef ^. nodeLoc) $ do
-        n <- get
-        let pid = portRef ^. portId
-            updateMode m = case m of
-                Normal         -> Highlighted
-                Invisible      -> Invisible
-                Inactive       -> Inactive
-                TypeNotMatched -> Highlighted
-                Highlighted    -> Highlighted
-                Moved pos      -> Moved pos
-                NameEdit       -> NameEdit
-            portModeLens = if not $ hasPort pid n
-                then argConstructorMode
-                else case pid of
-                    OutPortId' outpid -> outPortAt outpid . mode
-                    InPortId'  inpid  -> inPortAt  inpid  . mode
-        portModeLens %= updateMode
-
+    let nl  = portRef ^. nodeLoc
+        pid = portRef ^. portId
+        updateMode m = case m of
+            Normal         -> Highlighted
+            Invisible      -> Invisible
+            Inactive       -> Inactive
+            TypeNotMatched -> Highlighted
+            Highlighted    -> Highlighted
+            Moved pos      -> Moved pos
+            NameEdit       -> NameEdit
+        notBlocked = Set.null (Set.intersection actions actionsBlockingPortHighlight)
+    when notBlocked $ do
+        modifyExpressionNode nl $ do
+            n <- get
+            let portModeLens' = if hasPort pid n then portModeLens pid else argConstructorMode 
+            portModeLens' %= updateMode
+        modifyOutputNode nl $ portModeLens pid %= updateMode
 
 handleMouseLeave :: AnyPortRef -> Command State ()
 handleMouseLeave portRef = do
     let nl  = portRef ^. nodeLoc
         pid = portRef ^. portId
-    mayPortMode <- getExpressionNode nl >>= mapM (flip calculatePortMode pid)
-    withJust (mayPortMode) $ \m -> modifyExpressionNode nl $ do
-        portModeLens <- get >>= \n -> do
-            return $ if not $ hasPort pid n then argConstructorMode else case pid of
-                OutPortId' outpid -> outPortAt outpid . mode
-                InPortId'  inpid  -> inPortAt  inpid  . mode
-        portModeLens .= m
+    mayPortMode <- getExpressionNode nl >>= mapM (`calculatePortMode` pid)
+    withJust mayPortMode $ \m -> do
+        modifyExpressionNode nl $ do
+            n <- get
+            let portModeLens' = if hasPort pid n then portModeLens pid else argConstructorMode
+            portModeLens' .= m
+    modifyOutputNode nl $ portModeLens pid .= Normal
