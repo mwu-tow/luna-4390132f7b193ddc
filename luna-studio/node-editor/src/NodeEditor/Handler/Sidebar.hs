0ddfa7b0d19eb816812af635e8340acb6cde010e
LJK <leszek.kania@luna-lang.org>
2017-09-15 17:36:21 +0200

    Minor refactor of NodeEditor/Action/Sidebar.hs

--- luna-studio/node-editor/src/NodeEditor/Handler/Sidebar.hs
+++ luna-studio/node-editor/src/NodeEditor/Handler/Sidebar.hs
@@ -3,8 +3,9 @@ module NodeEditor.Handler.Sidebar where
 import           Common.Action.Command          (Command)
 import           Common.Prelude
 import           NodeEditor.Action.Basic        (toggleInputMode, toggleOutputMode)
+import qualified NodeEditor.Action.Batch        as Batch
 import           NodeEditor.Action.Searcher     (editPortName)
-import           NodeEditor.Action.Sidebar      (addPort, handleAppMove, handleMouseUp, handleSidebarMove, removePort)
+import           NodeEditor.Action.Sidebar      (handleAppMove, handleMouseUp, handleSidebarMove)
 import           NodeEditor.Event.Event         (Event (UI))
 import           NodeEditor.Event.UI            (UIEvent (AppEvent, SidebarEvent))
 import qualified NodeEditor.React.Event.App     as App
@@ -17,8 +18,8 @@ import           NodeEditor.State.Global        (State)
 handle :: Event -> Maybe (Command State ())
 handle (UI (SidebarEvent (Sidebar.ToggleInputMode   nl)))                         = Just $ toggleInputMode nl
 handle (UI (SidebarEvent (Sidebar.ToggleOutputMode  nl)))                         = Just $ toggleOutputMode nl
-handle (UI (SidebarEvent (Sidebar.RemovePort        (OutPortRef' portRef))))      = Just $ removePort portRef
-handle (UI (SidebarEvent (Sidebar.AddPort           (OutPortRef' portRef))))      = Just $ addPort portRef
+handle (UI (SidebarEvent (Sidebar.RemovePort        (OutPortRef' portRef))))      = Just $ Batch.removePort portRef
+handle (UI (SidebarEvent (Sidebar.AddPort           (OutPortRef' portRef))))      = Just $ Batch.addPort portRef Nothing
 handle (UI (AppEvent     (App.MouseMove             evt _)))                      = Just $ handleAppMove evt
 handle (UI (SidebarEvent (Sidebar.MouseMove         evt nodeLoc)))                = Just $ handleSidebarMove evt nodeLoc
 handle (UI (AppEvent     (App.MouseUp               evt)))                        = Just $ continue $ handleMouseUp evt
