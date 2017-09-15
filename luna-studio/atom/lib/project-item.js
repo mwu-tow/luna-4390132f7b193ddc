a368865c1c9f49bd024bff900452b18ab8e46ce4
Piotr Młodawski <pmlodawski@gmail.com>
2017-09-15 16:17:13 +0200

    Display animated luna logo, when downloading project

--- luna-studio/atom/lib/project-item.js
+++ luna-studio/atom/lib/project-item.js
@@ -1,5 +1,6 @@
 /** @babel */
 /** @jsx etch.dom */
+const Spinner = require('./spinner');
 
 var ProjectItem;
 
@@ -9,24 +10,48 @@ module.exports = ProjectItem = class ProjectItem {
     constructor(
             project,
             classes = "",
-            onOpen = (() => { return atom.project.setPaths([this.uri]); })) {
+            onOpen = ((progress, finalize) => {
+                progress(0.5);
+                atom.project.setPaths([this.uri]);
+                finalize();
+            })) {
         this.update = this.update.bind(this);
         this.render = this.render.bind(this);
+        this.thumbLogo = this.thumbLogo.bind(this);
         this.name = project.name;
         this.uri = project.uri != undefined ? project.uri : project.name;
         this.classes = classes;
-        this.onOpen = onOpen;
+        this.onOpen = function() {
+            spinner = new Spinner()
+            this.update( { logo: spinner.render() } );
+            etch.updateSync(this);
+            spinner.start();
+            etch.updateSync(this);
+            progress = function(p) {
+                // etch.updateSync(this);
+                spinner.setProgress(p);
+            };
+            var self = this;
+            finalize = function() {
+                self.update( { logo: self.thumbLogo() } );
+            }
+            onOpen(progress, finalize);
+        };
         this.thumb = project.thumb != undefined ? project.thumb : "atom://luna-studio/rsc/logo.png";
+        this.props = { logo: this.thumbLogo() }
         etch.initialize(this);
     }
 
     update(props, children) {
+        this.props = props;
         return etch.update(this);
     }
-
+    thumbLogo() {
+        return <img class="luna-project-logo" src={this.thumb}></img>
+    }
     render () {
         return  <div class={this.classes} on={{click: this.onOpen}}>
-                    <img class="luna-project-logo" src={this.thumb}></img>
+                    {this.props.logo}
                     <div class="luna-project-caption">{this.name}</div>
                 </div>
     }
