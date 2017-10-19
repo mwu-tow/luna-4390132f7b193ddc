4ba61b2a25635fdeefac934775cfc4a3c099840e
piotrMocz <piotr.moczurad@gmail.com>
2017-10-19 17:12:52 +0200

    Correct JSON logging in GUI

--- luna-manager/src/Luna/Manager/Logger.hs
+++ luna-manager/src/Luna/Manager/Logger.hs
@@ -9,12 +9,26 @@ import           Shelly.Lifted                (MonadSh, MonadShControl)
 import qualified Shelly.Lifted                as Sh
 import           System.IO                    (hFlush, stdout)
 
+import           Data.Aeson           (FromJSON, ToJSON, FromJSONKey, ToJSONKey, parseJSON, encode)
+import qualified Data.Aeson           as JSON
+import qualified Data.Aeson.Types     as JSON
+import qualified Data.Aeson.Encoding  as JSON
+import qualified Data.ByteString.Lazy as BS
+import           Control.Lens.Aeson
+
 import           Luna.Manager.Command.Options
 import           Luna.Manager.System.Env      (EnvConfig)
 import qualified Luna.Manager.System.Env      as System
 
 
+data WarningMessage = WarningMessage { message :: Text
+                                     } deriving (Show, Generic, Eq)
+
+instance ToJSON   WarningMessage
+instance FromJSON WarningMessage
+
 type LoggerMonad m = (MonadIO m, MonadSh m, MonadShControl m, MonadGetters '[Options, EnvConfig] m)
+
 
 logFilePath :: LoggerMonad m => m FilePath
 logFilePath = do
@@ -42,12 +56,15 @@ log msg = do
         gui  = opts ^. guiInstaller
     if verb && (not gui) then liftIO $ logToStdout msg else logToTmpFile msg
 
+logToJSON :: MonadIO m => Text -> m ()
+logToJSON = liftIO . print . encode . WarningMessage
+
 warning :: LoggerMonad m => Text -> m ()
 warning msg = do
     opts <- view globals <$> get @Options
     let verb = opts ^. verbose
         gui  = opts ^. guiInstaller
         m    = "WARNING: " <> msg
-    if   gui     then liftIO $ print $ "{\"message\": \"" <> m <> "\"}"
+    if   gui     then logToJSON m
     else if verb then liftIO $ logToStdout m
     else              logToTmpFile m
