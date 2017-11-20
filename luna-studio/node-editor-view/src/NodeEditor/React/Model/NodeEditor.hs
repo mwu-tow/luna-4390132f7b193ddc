5065862ffce5ae78a96455303f05760bd4c5afb1
LJK <leszek.kania@luna-lang.org>
2017-11-20 08:44:36 +0100

    Fix incorrect connections from pattern-match. #488 #343

--- luna-studio/node-editor-view/src/NodeEditor/React/Model/NodeEditor.hs
+++ luna-studio/node-editor-view/src/NodeEditor/React/Model/NodeEditor.hs
@@ -188,22 +188,21 @@ toPosConnection :: NodeEditor -> Connection -> Maybe PosConnection
 toPosConnection ne connection = do
     let src         = connection ^. Connection.src
         dst         = connection ^. Connection.dst
-        srcPortRef  = src & PortRef.srcPortId %~ atMostFirstLevel
         dstPortRef  = dst & PortRef.dstPortId %~ atMostFirstLevel
-        srcNodeLoc  = srcPortRef ^. PortRef.srcNodeLoc
+        srcNodeLoc  = src ^. PortRef.srcNodeLoc
         dstNodeLoc  = dstPortRef ^. PortRef.dstNodeLoc
         dstPortId   = dstPortRef ^. PortRef.dstPortId
         mode        = connection ^. Connection.mode
         sidebarConn = connection ^. Connection.sidebarConn
     srcNode <- getNode srcNodeLoc ne
     dstNode <- getNode dstNodeLoc ne
-    srcPort <- getPort srcPortRef ne
+    srcPort <- getPort src ne
     if hasPort dstPortId dstNode then do
         dstPort <- getPort dstPortRef ne
         (srcPos, dstPos) <- Connection.connectionPositions srcNode srcPort dstNode dstPort (ne ^. layout)
         return $ PosConnection src dst srcPos dstPos sidebarConn mode (srcPort ^. Port.color)
     else if countArgPorts dstNode == getPortNumber dstPortId then case dstNode of
-        Expression n -> fmap (Connection.toPosConnection srcPortRef dstPortRef) $
+        Expression n -> fmap (Connection.toPosConnection src dstPortRef) $
             toPosHalfConnection ne $ HalfConnection (OutPortRef' src) (Connection.argConstructorConnectionPos n) sidebarConn mode
         _            -> Nothing
     else Nothing
