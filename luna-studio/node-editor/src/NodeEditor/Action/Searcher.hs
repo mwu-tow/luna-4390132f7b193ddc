cd4a77078e12575c061dd0f9571d0f835c02d916
LJK <leszek.kania@luna-lang.org>
2017-11-20 07:14:13 +0100

    Do not allow to accept empty input in searcher. #475

--- luna-studio/node-editor/src/NodeEditor/Action/Searcher.hs
+++ luna-studio/node-editor/src/NodeEditor/Action/Searcher.hs
@@ -196,7 +196,7 @@ accept :: (Event -> IO ()) -> Searcher -> Command State ()
 accept scheduleEvent action = whenM (updateInputWithSelectedHint action) $
     withJustM getSearcher $ \searcher -> do
         let inputText = searcher ^. Searcher.inputText
-        case searcher ^. Searcher.mode of
+        if Text.null inputText then warning "Input cannot be empty." else case searcher ^. Searcher.mode of
             Searcher.Command                                             _ -> execCommand action scheduleEvent $ convert inputText
             Searcher.Node     nl (Searcher.NodeModeInfo _ (Just nn) _ _) _ -> createNode (nl ^. NodeLoc.path) (nn ^. Searcher.position) inputText False >> close action
             Searcher.Node     nl _                                       _ -> setNodeExpression nl inputText >> close action
