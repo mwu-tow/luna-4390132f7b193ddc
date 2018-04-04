9855b0fea6fdb156e0e63a6035e529d6de9a9f87
Wojciech Danilo <wojciech.danilo@gmail.com>
2018-04-04 05:01:50 +0200

    Updating the api

--- src/Data/Text/Span.hs
+++ src/Data/Text/Span.hs
@@ -4,10 +4,11 @@
 module Data.Text.Span where
 
 import qualified Control.Monad.State.Layered as State
+-- import qualified Foreign.Storable.Deriving   as Storable
 
 import Prologue            hiding (length)
 import Data.Maybe          (isJust)
-
+import Data.Text.Position (Delta)
 
 ----------------------------
 -- === LeftSpacedSpan === --
@@ -15,71 +16,78 @@ import Data.Maybe          (isJust)
 
 -- === Definition === --
 
-data    SpacedSpan      s = SpacedSpan      { __offset :: !s, __length :: !s } deriving (Show, Eq, Ord, Functor, Traversable, Foldable)
-newtype LeftSpacedSpan  s = LeftSpacedSpan  (SpacedSpan s) deriving (Show, Eq, Ord, Functor, Traversable, Foldable)
-newtype RightSpacedSpan s = RightSpacedSpan (SpacedSpan s) deriving (Show, Eq, Ord, Functor, Traversable, Foldable)
+data SpacedSpan = SpacedSpan 
+    { __offset :: !Delta
+    , __length :: !Delta 
+    } deriving (Eq, Ord, Show)
+-- Storable.derive ''SpacedSpan
+
+newtype LeftSpacedSpan  = LeftSpacedSpan  SpacedSpan deriving (Eq, Ord, Show)
+newtype RightSpacedSpan = RightSpacedSpan SpacedSpan deriving (Eq, Ord, Show)
 makeLenses ''SpacedSpan
 makeLenses ''LeftSpacedSpan
 makeLenses ''RightSpacedSpan
 
-class    IsSpacedSpan t               where spacedSpan :: forall s. Iso' (t s) (SpacedSpan s)
+class    IsSpacedSpan t               where spacedSpan :: Iso' t SpacedSpan
 instance IsSpacedSpan LeftSpacedSpan  where spacedSpan = wrapped
 instance IsSpacedSpan RightSpacedSpan where spacedSpan = wrapped
 
 
 -- === Construction === --
 
-leftSpacedSpan :: s -> s -> LeftSpacedSpan s
-leftSpacedSpan = LeftSpacedSpan .: SpacedSpan
-
-rightSpacedSpan :: s -> s -> RightSpacedSpan s
-rightSpacedSpan = RightSpacedSpan .: SpacedSpan
+leftSpacedSpan  :: Delta -> Delta -> LeftSpacedSpan
+rightSpacedSpan :: Delta -> Delta -> RightSpacedSpan
+leftSpacedSpan  = LeftSpacedSpan  .: SpacedSpan ; {-# INLINE leftSpacedSpan #-}
+rightSpacedSpan = RightSpacedSpan .: SpacedSpan ; {-# INLINE rightSpacedSpan #-}
 
 
 -- === Props === --
 
-offset, length :: IsSpacedSpan t => Lens' (t s) s
-offset = spacedSpan . spacedSpan_offset
-length = spacedSpan . spacedSpan_length
+offset, length :: IsSpacedSpan t => Lens' t Delta
+offset = spacedSpan . spacedSpan_offset ; {-# INLINE offset #-}
+length = spacedSpan . spacedSpan_length ; {-# INLINE length #-}
 
-asOffsetSpan :: (IsSpacedSpan t, Num s) => t s -> t s
+asOffsetSpan :: IsSpacedSpan t => t -> t
 asOffsetSpan s = s & offset %~ (+ s ^. length)
                    & length .~ 0
+{-# INLINE asOffsetSpan #-}
 
-measure :: (IsSpacedSpan t, Semigroup s) => t s -> s
-measure t = t ^. offset <> t ^. length
+measure :: IsSpacedSpan t => t -> Delta
+measure t = t ^. offset + t ^. length ; {-# INLINE measure #-}
 
--- | `concat` is the most primitive concatenation method, which does not treat offset only left spans in special way
-concat :: Num s => LeftSpacedSpan s -> LeftSpacedSpan s -> LeftSpacedSpan s
-concat (unwrap -> SpacedSpan off s) (unwrap -> SpacedSpan off' s') = wrap $ SpacedSpan off (s + off' + s')
+concat :: Num s => LeftSpacedSpan -> LeftSpacedSpan -> LeftSpacedSpan
+concat (unwrap -> SpacedSpan off s) (unwrap -> SpacedSpan off' s') 
+    = wrap $ SpacedSpan off (s + off' + s')
 
 
 -- === Instances === --
 
-instance Num s => Default (SpacedSpan s) where def    = SpacedSpan 0 0
-instance Num s => Mempty  (SpacedSpan s) where mempty = SpacedSpan 0 0
+instance Num s => Default SpacedSpan where def    = SpacedSpan 0 0
+instance Num s => Mempty  SpacedSpan where mempty = SpacedSpan 0 0
 
-deriving instance Num s => Default (LeftSpacedSpan  s)
-deriving instance Num s => Default (RightSpacedSpan s)
-deriving instance Num s => Mempty  (LeftSpacedSpan  s)
-deriving instance Num s => Mempty  (RightSpacedSpan s)
+deriving instance Num s => Default LeftSpacedSpan
+deriving instance Num s => Default RightSpacedSpan
+deriving instance Num s => Mempty  LeftSpacedSpan
+deriving instance Num s => Mempty  RightSpacedSpan
 
-instance (Num s, Ord s) => Semigroup (LeftSpacedSpan s) where
-    (unwrap -> SpacedSpan off s) <> (unwrap -> SpacedSpan off' s') = if s > 0 then leftSpacedSpan off (s + off' + s')
-                                                                              else leftSpacedSpan (off + off') s'
+instance (Num s, Ord s) => Semigroup LeftSpacedSpan where
+    (unwrap -> SpacedSpan off s) <> (unwrap -> SpacedSpan off' s') 
+        = if s > 0 then leftSpacedSpan off (s + off' + s')
+                   else leftSpacedSpan (off + off') s'
 
-instance (Num s, Ord s) => Semigroup (RightSpacedSpan s) where
-    (unwrap -> SpacedSpan s off) <> (unwrap -> SpacedSpan s' off') = if s' > 0 then rightSpacedSpan (s + off + s') off'
-                                                                               else rightSpacedSpan s (off + off')
+instance (Num s, Ord s) => Semigroup RightSpacedSpan where
+    (unwrap -> SpacedSpan s off) <> (unwrap -> SpacedSpan s' off')
+        = if s' > 0 then rightSpacedSpan (s + off + s') off'
+                    else rightSpacedSpan s (off + off')
 
 -- Conversions
-instance Convertible (SpacedSpan s)      (s,s)               where convert (SpacedSpan o l) = (o,l)
-instance Convertible (s,s)               (SpacedSpan s)      where convert (o,l) = (SpacedSpan o l)
-instance Convertible (SpacedSpan s)      (LeftSpacedSpan  s) where convert = wrap
-instance Convertible (SpacedSpan s)      (RightSpacedSpan s) where convert = wrap
-instance Convertible (LeftSpacedSpan  s) (SpacedSpan s)      where convert = unwrap
-instance Convertible (RightSpacedSpan s) (SpacedSpan s)      where convert = unwrap
-instance Convertible (LeftSpacedSpan  s) (s,s)               where convert = convertVia @(SpacedSpan s)
-instance Convertible (RightSpacedSpan s) (s,s)               where convert = convertVia @(SpacedSpan s)
-instance Convertible (s,s)               (LeftSpacedSpan  s) where convert = convertVia @(SpacedSpan s)
-instance Convertible (s,s)               (RightSpacedSpan s) where convert = convertVia @(SpacedSpan s)
+instance Convertible SpacedSpan      (Delta,Delta)   where convert (SpacedSpan o l) = (o,l)
+instance Convertible (Delta,Delta)   SpacedSpan      where convert (o,l) = (SpacedSpan o l)
+instance Convertible SpacedSpan      LeftSpacedSpan  where convert = wrap
+instance Convertible SpacedSpan      RightSpacedSpan where convert = wrap
+instance Convertible LeftSpacedSpan  SpacedSpan      where convert = unwrap
+instance Convertible RightSpacedSpan SpacedSpan      where convert = unwrap
+instance Convertible LeftSpacedSpan  (Delta,Delta)   where convert = convertVia @SpacedSpan
+instance Convertible RightSpacedSpan (Delta,Delta)   where convert = convertVia @SpacedSpan
+instance Convertible (Delta,Delta)   LeftSpacedSpan  where convert = convertVia @SpacedSpan
+instance Convertible (Delta,Delta)   RightSpacedSpan where convert = convertVia @SpacedSpan
