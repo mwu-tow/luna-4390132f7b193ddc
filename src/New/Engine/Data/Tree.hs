57bc442a4ba7aa9a68cf40d6db05bd25a32c2bd0
LJK <kanialj@gmail.com>
2018-11-26 14:25:35 +0100

    CR fixes and refactor

--- src/New/Engine/Data/Tree.hs
+++ src/New/Engine/Data/Tree.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE Strict #-}
 module New.Engine.Data.Tree where
 
-import Prologue
+import Prologue hiding (Index)
 
 import qualified Control.Monad.State.Layered as State
 import qualified Data.List                   as List
@@ -9,40 +9,42 @@ import qualified Data.Map                    as Map
 import qualified Data.Text                   as Text
 
 import Control.Arrow   ((&&&))
-import Control.Lens    ((?~))
+import Control.Lens    (to, (?~), _Just)
 import Data.Map        (Map)
 import New.Engine.Data (SearcherData, text)
 
 
-----------------
--- === ID === --
-----------------
+-------------------
+-- === Index === --
+-------------------
 
 -- === Definition === --
 
-newtype ID = ID Int deriving (Eq, Show)
+newtype Index = Index Int deriving (Eq, Generic, Num, Ord, Show)
+makeClassy ''Index
 
-notExists :: ID
-notExists = ID $ -1
+notExists :: Index
+notExists = -1
 
-instance Default ID where def = notExists
+instance Default Index where def = notExists
+instance NFData  Index
 
 -- === Utils === --
 
-isInvalid :: ID -> Bool
-isInvalid (ID i) = i < 0
+isInvalid :: Index -> Bool
+isInvalid = (< 0)
 
-getID :: State.Monad ID m => m ID
-getID = State.modify @ID (\(ID i) -> let x = ID $ i + 1 in (x,x))
-{-# INLINE getID #-}
+getIndex :: State.Monad Index m => m Index
+getIndex = State.modify @Index (\prev -> let curr = prev + 1 in (curr,curr))
+{-# INLINE getIndex #-}
 
--------------------
--- === IDMap === --
--------------------
+----------------------
+-- === IndexMap === --
+----------------------
 
 -- === Definition === --
 
-type IDMap = Map Text ID
+type IndexMap = Map Text Index
 
 
 ------------------
@@ -52,13 +54,15 @@ type IDMap = Map Text ID
 -- === Definition === --
 
 data Node = Node
-    { _nodeId   :: {-# UNPACK #-} !ID
+    { __index   :: {-# UNPACK #-} !Index
     , _branches :: !(Map Char Node)
-    } deriving (Eq, Show)
+    } deriving (Eq, Generic, Show)
 
 makeLenses ''Node
 
-instance Default Node where def = Node def mempty
+instance Default  Node where def   = Node def mempty
+instance HasIndex Node where index = node_index
+instance NFData   Node
 
 -- === API === --
 
@@ -69,14 +73,15 @@ insert sd n = insertKeyed (sd ^. text) sd n where
         Nothing -> updateValue v node
         Just c  -> insertAtChar c (Text.drop 1 k) v node
     updateValue :: TreeContext m a => a -> Node -> m Node
-    updateValue v node = if not . isInvalid $ node ^. nodeId
+    updateValue v node = if not . isInvalid $ node ^. index
         then pure node
-        else getID >>= \newId -> do
-            State.modify_ @IDMap $ Map.insert (v ^. text) newId
-            pure $ node & nodeId .~ newId
+        else do
+            newIndex <- getIndex
+            State.modify_ @IndexMap $ Map.insert (v ^. text) newIndex
+            pure $ node & index .~ newIndex
     insertAtChar :: TreeContext m a => Char -> Text -> a -> Node -> m Node
-    insertAtChar c k v node = 
-        let update v = node & branches . at c ?~ v
+    insertAtChar c k v node =
+        let update val = node & branches . at c ?~ val
         in update <$> insertKeyed k v (fromJust def $ node ^. branches . at c)
 
 insertMultiple :: TreeContext m a => [a] -> Node -> m Node
@@ -89,40 +94,50 @@ insertMultiple input n = addToNode (keyed input) n where
     updateBranches :: TreeContext m a => [(Text, a)] -> Node -> m Node
     updateBranches sd node = do
         let processData acc c v = do
-                updated <- addToNode
-                    v
+                updated <- addToNode v
                     $ fromJust def $ node ^. branches . at c
                 pure $ Map.insert c updated acc
         newBranches <- foldlMWithKey processData mempty $ groupByFirstLetter sd
         pure $ node & branches .~ newBranches
     updateValue :: TreeContext m a => [a] -> Node -> m Node
-    updateValue sd node = if (not . isInvalid $ node ^. nodeId) || null sd
+    updateValue sd node = if (not . isInvalid $ node ^. index) || null sd
         then pure node
-        else getID >>= \newId -> do
-            State.modify_ @IDMap $ Map.insert ((unsafeHead sd) ^. text) newId
-            pure $ node & nodeId .~ newId
+        else do
+            newIndex <- getIndex
+            let withText    = for_ (sd ^? to head . _Just . text)
+                updateMap t = State.modify_ @IndexMap $ Map.insert t newIndex
+            withText updateMap
+            pure $ node & index .~ newIndex
 
 -- === Utils ==== --
 
-type TreeContext m a = (SearcherData a, State.Monad ID m, State.Monad IDMap m)
+type TreeContext m a =
+    ( SearcherData a
+    , State.Monad Index m
+    , State.Monad IndexMap m )
 
 textHead :: Text -> Maybe Char
 textHead t = if Text.null t then Nothing else Just $ Text.head t
+{-# INLINE textHead #-}
 
 foldlMWithKey :: Monad m => (a -> k -> b -> m a) -> a -> Map k b -> m a
-foldlMWithKey f init = Map.foldlWithKey
+foldlMWithKey f initVal = Map.foldlWithKey
     (\acc k v -> acc >>= \a -> f a k v)
-    (pure init)
+    (pure initVal)
+{-# INLINE foldlMWithKey #-}
 
 groupByFirstLetter :: [(Text, a)] -> Map Char [(Text,a)]
 groupByFirstLetter sd = foldl
     (\acc (k, v) -> Map.insertWith (<>) k (pure v) acc)
     mempty
     $ mapMaybe detachHead sd
+{-# INLINE groupByFirstLetter #-}
 
 keyed :: SearcherData a => [a] -> [(Text, a)]
 keyed = List.sortOn fst . fmap (view text &&& id)
+{-# INLINE keyed #-}
 
 detachHead :: (Text, a) -> Maybe (Char, (Text, a))
 detachHead (k, v) = (, (Text.drop 1 k, v)) <$> textHead k
+{-# INLINE detachHead #-}
 
--- test/TreeSpec.hs
+++ test/TreeSpec.hs
@@ -1,23 +1,22 @@
 module TreeSpec (spec) where
 
-import Prologue
+import Prologue   hiding (Index)
 import Test.Hspec
 
 import qualified Control.Monad.State.Layered as State
 import qualified Data.Map                    as Map
 import qualified Data.Text                   as Text
-import qualified New.Engine.Data.Tree  as Tree
+import qualified New.Engine.Data.Tree        as Tree
 
-import Control.Lens (makePrisms)
 import Control.Exception    (throw)
-import Data.Map             (Map)
+import Control.Lens         (makePrisms)
 import Data.Set             (Set)
-import New.Engine.Data      (SearcherData, text)
-import New.Engine.Data.Tree (ID (ID), IDMap, Node (Node), branches, nodeId)
+import New.Engine.Data.Tree (Index (Index), IndexMap, Node (Node), branches,
+                             index)
 
 
 data TreeStructureExceptionType
-    = IncorrectId       ID         ID
+    = IncorrectIndex    Index      Index
     | IncorrectBranches (Set Char) (Set Char)
     deriving (Eq, Generic, Show, Typeable)
 
@@ -32,8 +31,8 @@ makePrisms ''TreeStructureExceptionType
 instance Exception TreeStructureException
 
 
-recursiveCheckTreeStructure :: Text -> IDMap -> Node -> IO ()
-recursiveCheckTreeStructure matchedPrefix idMap dict = check where
+recursiveCheckTreeStructure :: Text -> IndexMap -> Node -> IO ()
+recursiveCheckTreeStructure matchedPrefix indexMap dict = check where
     accFunction acc k v = if Text.null k
         then acc
         else Map.insertWith
@@ -41,25 +40,26 @@ recursiveCheckTreeStructure matchedPrefix idMap dict = check where
             (Text.head k)
             (Map.singleton (Text.drop 1 k) v)
             acc
-    slicedMap = Map.foldlWithKey accFunction mempty idMap
-    currentId = fromJust (ID $ -1) $ Map.lookup Text.empty idMap
+    slicedMap = Map.foldlWithKey accFunction mempty indexMap
+    currentIndex = fromJust def $ Map.lookup mempty indexMap
     checkForException :: (Eq a, Show a, Typeable a)
         => (a -> a -> TreeStructureExceptionType) -> a -> a -> IO ()
     checkForException tpe a b = when (a /= b) $ throw
         $ TreeStructureException matchedPrefix $ tpe a b
     check   = do
-        checkForException IncorrectId currentId $ dict ^. nodeId
+        checkForException IncorrectIndex currentIndex $ dict ^. index
         checkForException
             IncorrectBranches
             (Map.keysSet slicedMap)
             (Map.keysSet $ dict ^. branches)
-        forM_ (toList slicedMap) $ \(c, newIdMap) -> withJust
-            (dict ^. branches . at c)
-            $ recursiveCheckTreeStructure (Text.snoc matchedPrefix c) newIdMap
+        for_ (toList slicedMap) $ \(c, newIndexMap) -> 
+            for_ (dict ^. branches . at c) $ recursiveCheckTreeStructure 
+                (Text.snoc matchedPrefix c) 
+                newIndexMap
 
-checkTreeStructure :: IDMap -> Node -> IO ()
-checkTreeStructure idMap dict = catch check handleException where
-    check = recursiveCheckTreeStructure mempty idMap dict
+checkTreeStructure :: IndexMap -> Node -> IO ()
+checkTreeStructure indexMap dict = catch check handleException where
+    check = recursiveCheckTreeStructure mempty indexMap dict
     handleException :: TreeStructureException -> IO ()
     handleException e =
         let k = e ^. dictionaryKey
@@ -67,7 +67,7 @@ checkTreeStructure idMap dict = catch check handleException where
             expectEq m d = (k, m) `shouldBe` (k, d)
         in case e ^. exceptionType of
             IncorrectBranches m d -> expectEq m d
-            IncorrectId       m d -> expectEq m d
+            IncorrectIndex    m d -> expectEq m d
 
 
 dictionaryStructureExceptionSelector :: Selector TreeStructureException
@@ -78,75 +78,75 @@ spec = do
     describe "tree structure check tests" $ do
         it "works on empty tree" $ checkTreeStructure mempty def
         it "works with single letter" $ checkTreeStructure
-            (Map.singleton "a" $ ID 0)
-            $ Node (ID $ -1) $ Map.singleton 'a' $ Node (ID 0) mempty
+            (Map.singleton "a" 0)
+            $ Node def $ Map.singleton 'a' $ Node 0 mempty
         it "works with branched data" $ checkTreeStructure
-            (Map.fromList [("aa", ID 0), ("ab", ID 1)])
-            $ Node (ID $ -1) $ Map.singleton
-                'a' $ Node (ID $ -1) $ Map.fromList
-                    [ ('a', Node (ID 0) mempty)
-                    , ('b', Node (ID 1) mempty) ]
+            (Map.fromList [("aa", 0), ("ab", 1)])
+            $ Node def $ Map.singleton
+                'a' $ Node def $ Map.fromList
+                    [ ('a', Node 0 mempty)
+                    , ('b', Node 1 mempty) ]
         it "throws exception when map empty and dict not empty" $ shouldThrow
             (recursiveCheckTreeStructure
                 mempty
                 mempty
-                $ Node (ID $ -1) $ Map.singleton 'a' $ Node (ID 0) mempty)
+                $ Node def $ Map.singleton 'a' $ Node 0 mempty)
             dictionaryStructureExceptionSelector
         it "throws exception when map not empty and dict empty" $ shouldThrow
-            (recursiveCheckTreeStructure mempty (Map.singleton "a" $ ID 0) def)
+            (recursiveCheckTreeStructure mempty (Map.singleton "a" 0) def)
             dictionaryStructureExceptionSelector
         it "throws exception when map does not match dict" $ shouldThrow
             (recursiveCheckTreeStructure
                 mempty
-                (Map.singleton "ab" $ ID 0)
-                $ Node (ID $ -1) $ Map.singleton
-                'a' $ Node (ID $ -1) $ Map.fromList
-                    [ ('a', Node (ID 0) mempty)
-                    , ('b', Node (ID 1) mempty) ])
+                (Map.singleton "ab" 0)
+                $ Node def $ Map.singleton
+                'a' $ Node def $ Map.fromList
+                    [ ('a', Node 0 mempty)
+                    , ('b', Node 1 mempty) ])
             dictionaryStructureExceptionSelector
     describe "test insert function" $ do
         it "value is in map" $
-            let perform = State.execDefT @IDMap . State.evalDefT @ID
+            let perform = State.execDefT @IndexMap . State.evalDefT @Index
             in perform (Tree.insert (Text.pack "a") def) >>=
-                shouldBe (Map.singleton (Text.pack "a") $ ID 0)
+                shouldBe (Map.singleton (Text.pack "a") 0)
         it "value is in dictionary" $
-            let perform = State.evalDefT @IDMap . State.evalDefT @ID
+            let perform = State.evalDefT @IndexMap . State.evalDefT @Index
             in perform (Tree.insert (Text.pack "a") def) >>=
-                checkTreeStructure (Map.singleton (Text.pack "a") $ ID 0)
+                checkTreeStructure (Map.singleton (Text.pack "a") $ Index 0)
         it "values are in map" $
-            let perform = State.execDefT @IDMap . State.evalDefT @ID
+            let perform = State.execDefT @IndexMap . State.evalDefT @Index
                 action  = Tree.insert (Text.pack "aa") def
                     >>= Tree.insert (Text.pack "ab")
             in perform action >>= shouldBe
-                (Map.fromList [(Text.pack "aa", ID 0), (Text.pack "ab", ID 1)])
+                (Map.fromList [(Text.pack "aa", 0), (Text.pack "ab", 1)])
         it "values are in dictionary" $
-            let perform = State.evalDefT @IDMap . State.evalDefT @ID
+            let perform = State.evalDefT @IndexMap . State.evalDefT @Index
                 action  = Tree.insert (Text.pack "aa") def
                     >>= Tree.insert (Text.pack "ab")
-            in perform action >>= checkTreeStructure (Map.fromList 
-                [ (Text.pack "aa", ID 0)
-                , (Text.pack "ab", ID 1) ])
+            in perform action >>= checkTreeStructure (Map.fromList
+                [ (Text.pack "aa", 0)
+                , (Text.pack "ab", 1) ])
     describe "test insertMultiple function" $ do
         it "value is in map" $
-            let perform = State.execDefT @IDMap . State.evalDefT @ID
+            let perform = State.execDefT @IndexMap . State.evalDefT @Index
             in perform (Tree.insertMultiple [Text.pack "a"] def) >>=
-                shouldBe (Map.singleton (Text.pack "a") $ ID 0)
+                shouldBe (Map.singleton (Text.pack "a") 0)
         it "value is in dictionary" $
-            let perform = State.evalDefT @IDMap . State.evalDefT @ID
+            let perform = State.evalDefT @IndexMap . State.evalDefT @Index
             in perform (Tree.insertMultiple [Text.pack "a"] def) >>=
-                checkTreeStructure (Map.singleton (Text.pack "a") $ ID 0)
+                checkTreeStructure (Map.singleton (Text.pack "a") 0)
         it "values are in map" $
-            let perform = State.execDefT @IDMap . State.evalDefT @ID
+            let perform = State.execDefT @IndexMap . State.evalDefT @Index
                 action  = Tree.insertMultiple
                     [Text.pack "aa", Text.pack "ab"]
                     def
             in perform action >>= shouldBe
-                (Map.fromList [(Text.pack "aa", ID 0), (Text.pack "ab", ID 1)])
+                (Map.fromList [(Text.pack "aa", 0), (Text.pack "ab", 1)])
         it "values are in dictionary" $
-            let perform = State.evalDefT @IDMap . State.evalDefT @ID
+            let perform = State.evalDefT @IndexMap . State.evalDefT @Index
                 action  = Tree.insertMultiple
                     [Text.pack "aa", Text.pack "ab"]
                     def
-            in perform action >>= checkTreeStructure (Map.fromList 
-                [ (Text.pack "aa", ID 0)
-                , (Text.pack "ab", ID 1) ])
+            in perform action >>= checkTreeStructure (Map.fromList
+                [ (Text.pack "aa", 0)
+                , (Text.pack "ab", 1) ])
